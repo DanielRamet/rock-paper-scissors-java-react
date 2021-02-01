74fb15ef03666669ce130987e2cd332f10ac85da
Xapaya <drametbarea@gmail.com>
2021-02-01 09:31:11 +0100

    - Blank Project created (using spring initializer)

---
+++ src/main/java/com/dramet/rockpaperscissorsjavareact/RockPaperScissorsJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.dramet.rockpaperscissorsjavareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsJavaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsJavaReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dramet/rockpaperscissorsjavareact/RockPaperScissorsJavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dramet.rockpaperscissorsjavareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsJavaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
